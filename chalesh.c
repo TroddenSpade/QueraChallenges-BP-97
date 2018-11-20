39027f21869a776720a61712669227369f3e6d85
Parsa Sam <parsasam1378@gmail.com>
2018-11-21 00:10:29 +0330

    Update chalesh.c

--- chalesh.c
+++ chalesh.c
@@ -1,73 +1,49 @@
 #include<stdio.h>
 #include<math.h>
 
-double function(double x,double a ,double b,double c,double d, double e)
-{
-    return a*pow(x,4) + b*pow(x,3) + c*pow(x,2) + d*x + e;
-}
-
-double dFunction (double x,double a ,double b,double c,double d)
-{
-    return a*4*pow(x,3) + b*3*pow(x,2) + c*2*x + d;
-}
-
-double newton(double x_0,double a ,double b,double c,double d,double e){
-    double x,temp;
-    double khata = 0.001;
-    for (int i=1; ; i++)
-    {
-        temp=function(x_0,a,b,c,d,e)/dFunction(x_0,a,b,c,d);
-        x=x_0-temp;
-        if (fabs(temp) < khata)
-        {
-            return x;
-        }
-        x_0=x;
-    }
-    return 0;
-}
-
-double disLine(double x ,double y , double a , double b){
-    return fabs(y - a*x - b)/a ;
-}
-
-
 int main()
 {
-    double a,b,c,d,e;
-    double distance;
-    double dL , dW ;
+    
     double vL , vW;
-    double x1,y1 ,x2,y2;
-    double aP,bP;
+    double r1,s1 ,r2,s2;
+    double a,b;
+    double x1,x0,x2;
+    double sqL0 , sqW0 ,sqL1 ,sqW1;
+    double f0,f1;
+    double result;
 
     scanf("%lf%lf",&vL,&vW);
-    scanf("%lf%lf",&x1,&y1);
-    scanf("%lf%lf",&x2,&y2);
-    scanf("%lf%lf",&aP,&bP);
+    scanf("%lf%lf",&r1,&s1);
+    scanf("%lf%lf",&r2,&s2);
+    scanf("%lf%lf",&a,&b);
 
+    x0 = r1;
+    x1 = r2;
+    
+    
+    for (int i=1; i <=40 ; i++)
+    {
+        sqL0 =sqrt( pow(x0-r1,2) + pow(a*x0 +b-s1 ,2) );
+        sqW0 =sqrt( pow(x0-r2,2) + pow(a*x0 +b-s2 ,2) );
+        sqL1 =sqrt( pow(x1-r1,2) + pow(a*x1 +b-s1 ,2) );
+        sqW1 =sqrt( pow(x1-r2,2) + pow(a*x1 +b-s2 ,2) );
+        
+        
 
-    dL = disLine(x1,y1,aP,bP);
-    dW = disLine(x2,y2,aP,bP);
+        f0 = ((a*a +1)*x0 +a*b-a*s1-r1 )/(vL*sqL0) + ((a*a +1)*x0 +a*b-a*s2-r2)/(vW*sqW0);
+        f1 = ((a*a +1)*x1 +a*b-a*s1-r1 )/(vL*sqL1) + ((a*a +1)*x1 +a*b-a*s2-r2)/(vW*sqW1);
+    
 
-    distance = sqrt(pow(x1-x2,2)+pow(y1-y2,2)-pow(dL+dW,2));
+        x2=(x0*f1-x1*f0)/(f1-f0);
+        if (f1-f0 == 0){
+            break;
+        }
+        x0=x1;
+        x1=x2;
+        
+    }
 
-    vL = vL*vL;
-    vW = vW*vW;
-    dL = dL*dL;
-    dW = dW*dW;
-
-    a = vW - vL;
-    b = (vL-vW)*2*distance;
-    c = (dL+distance*distance)*vW -(dW +distance*distance)*vL;
-    d = 2*distance*vL*dW;
-    e = vL*dW*distance*distance;
-
-
-    printf("%f",newton(distance/2 , a,b,c,d,e));
-
-
-
+    printf("%f\n",sqL1/vL + sqW1/vW);
 
 
 
