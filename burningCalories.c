58d52e439915633d6d0e86f2fb39d0fc12275558
Parsa Sam <parsasam1378@gmail.com>
2018-12-10 14:20:02 +0330

    100

--- burningCalories.c
+++ burningCalories.c
@@ -1,41 +1,48 @@
 #include <stdio.h>
 
-int maxRout(int m,int n,int array[n][m],int x,int y,int direc);
+void maxRout(int m,int n,int array[n][m],int max[n][m],int direc);
 
 int main(){
     int m ,n;
-    scanf("%d%d",&m,&n);
-    // m tool , n arz
+    scanf("%d%d",&n,&m);
+    // n tool , m arz
     int a[n][m];
+
+    // Right-Down => 0 = direct
+    // Right-Up => 1 = direct
+    // Left-Up => 11 = direct
+    // Left-Down => 10 = direct
+    int rd[n][m];
+    int ru[n][m];
+    int lu[n][m];
+    int ld[n][m];
     
     for(int i=0;i<n;i++){
         for(int j=0;j<m;j++){
             scanf("%d",&a[i][j]);
         }
     }
+    maxRout(m,n,a,lu,11);
+    maxRout(m,n,a,rd,0);
+    maxRout(m,n,a,ru,1);
+    maxRout(m,n,a,ld,10);
 
-    // Right-Down => 0 = direct
-    // Right-Up => 1 = direct
-    // Left-Up => 11 = direct
-    // Left-Down => 10 = direct
     int max1, max2, maxi,maxj;
     int result =0;
     int max;
     for(int i=1;i<m-1;i++){// soton = x
         for(int j=1;j<n-1;j++){// satr = y
             // khat amodi
-            max1 = maxRout(m,n,a,i,j-1,11) + maxRout(m,n,a,i,j+1,0) + maxRout(m,n,a,i+1,j,1) + maxRout(m,n,a,i-1,j,10);
+            max1 = lu[j-1][i] + rd[j+1][i] + ru[j][i+1] + ld[j][i-1];
             // khat ofoghi
-            max2 = maxRout(m,n,a,i,j-1,1) + maxRout(m,n,a,i,j+1,10) + maxRout(m,n,a,i+1,j,0) + maxRout(m,n,a,i-1,j,11);
+            max2 = ru[j-1][i] + ld[j+1][i] + rd[j][i+1] + lu[j][i-1];
 
             max = max1 > max2 ? max1 : max2 ;
 
-            if(max > result){    result = max;  maxi =i;  maxj = j;    }
-        
+            if (result < max)  result = max ;
+
         }
     }
-    int i = maxi;
-    int j = maxj;
     
     printf("%d",result);
 
@@ -43,77 +50,76 @@ int main(){
 }
 
 // x , y bar Ax
-int maxRout(int m,int n,int array[n][m],int x,int y,int direc){
+void maxRout(int m,int n,int array[n][m],int max[n][m],int direc){
     if (direc == 1){// Right-Up///////////////////////////////
-    int sum =0;
-    if (y == 0){
-        for(int i=x;i<m;i++){
-            sum += array[0][i];
-        }
-        return sum;
+    for(int i=0;i<n;i++){
+        if(i == 0)  max[i][m-1]=array[i][m-1];
+        else    max[i][m-1] = max[i-1][m-1]+array[i][m-1];
     }
-    if(x == m-1){
-        for(int i=y;i>=0;i--){
-            sum += array[i][m-1];
-        }
-        return sum;
+    for(int i=m-1;i>=0;i--){
+        if(i == m-1)  max[0][i]=array[0][i];
+        else    max[0][i] = max[0][i+1]+array[0][i];
     }
-    int max1 = maxRout(m,n,array,x,y-1,1) + array[y][x];
-    int max2 = maxRout(m,n,array,x+1,y,1) + array[y][x];
-    return max1 > max2 ? max1 : max2 ;
+
+    for(int i=m-2;i>=0;i--){// soton = x
+        for(int j=1;j<=n-1;j++){// satr = y
+            int maxA = max[j-1][i] > max[j][i+1] ? max[j-1][i] : max[j][i+1] ;
+            max[j][i] = array[j][i]+ maxA;
+        }
+    }
+
 
     }else if(direc == 0){// Right-Down////////////////////////
-    int sum =0;
-    if (y == n-1){
-        for(int i=x;i<m;i++){
-            sum += array[n-1][i];
-        }
-        return sum;
+    for(int i=n-1;i>=0;i--){// y
+        if(i == n-1)  max[i][m-1]=array[i][m-1];
+        else    max[i][m-1] = max[i+1][m-1]+array[i][m-1];
     }
-    if(x == m-1){
-        for(int i=y;i<n;i++){
-            sum += array[i][m-1];
-        }
-        return sum;
+    for(int i=m-1;i>=0;i--){// x
+        if(i == m-1)  max[n-1][i]=array[n-1][i];
+        else    max[n-1][i] = max[n-1][i+1]+array[n-1][i];
     }
-    int max1 = maxRout(m,n,array,x,y+1,0) + array[y][x];
-    int max2 = maxRout(m,n,array,x+1,y,0) + array[y][x];
-    return max1 > max2 ? max1 : max2 ;
+
+    for(int i=m-2;i>=0;i--){// soton = x
+        for(int j=n-2;j>=0;j--){// satr = y
+            int maxA = max[j+1][i] > max[j][i+1] ? max[j+1][i] : max[j][i+1] ;
+            max[j][i] = array[j][i]+ maxA;
+        }
+    }
+
 
     }else if(direc == 10){// Left-Down/////////////////////////
-    int sum =0;
-    if (y == n-1){
-        for(int i=x;i>=0;i--){
-            sum += array[n-1][i];
-        }
-        return sum;
+
+    max[n-1][0]=array[n-1][0];
+    for(int i=n-2;i>=0;i--){// y
+        max[i][0] = max[i+1][0] + array[i][0];
     }
-    if(x == 0){
-        for(int i=y;i<n;i++){
-            sum += array[i][0];
-        }
-        return sum;
+    for(int i=1;i<m;i++){// x
+        max[n-1][i] = max[n-1][i-1] + array[n-1][i];
     }
-    int max1 = maxRout(m,n,array,x,y+1,10) + array[y][x];
-    int max2 = maxRout(m,n,array,x-1,y,10) + array[y][x];
-    return max1 > max2 ? max1 : max2 ;
+
+    for(int i=1;i<=m-1;i++){// soton = x
+        for(int j=n-2;j>=0;j--){// satr = y
+            int maxA = max[j+1][i] > max[j][i-1] ? max[j+1][i] : max[j][i-1] ;
+            max[j][i] = array[j][i]+ maxA;
+        }
+    }
 
     }else{// Left-Up//////////////////////////////////////////
-    int sum =0;
-    if (y == 0){
-        for(int i=x;i>=0;i--){
-            sum += array[0][i];
-        }
-        return sum;
+    for(int i=0;i<n;i++){ // y
+        if(i == 0)  max[i][0]=array[i][0];
+        else    max[i][0] = max[i-1][0]+array[i][0];
     }
-    if(x == 0){
-        for(int i=y;i>=0;i--){
-            sum += array[i][0];
-        }
-        return sum;
+    for(int i=0;i<m;i++){// x
+        if(i == 0)  max[0][i]=array[0][i];
+        else    max[0][i] = max[0][i-1]+array[0][i];
     }
-    int max1 = maxRout(m,n,array,x,y-1,11) + array[y][x];
-    int max2 = maxRout(m,n,array,x-1,y,11) + array[y][x];
-    return max1 > max2 ? max1 : max2 ;
+
+    for(int i=1;i<=m-1;i++){// soton = x
+        for(int j=1;j<=n-1;j++){// satr = y
+            int maxA = max[j-1][i] > max[j][i-1] ? max[j-1][i] : max[j][i-1] ;
+            max[j][i] = array[j][i]+ maxA;
+        }
+    }
+
     }
 }
